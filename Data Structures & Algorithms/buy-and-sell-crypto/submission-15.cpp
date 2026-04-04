79f626112ce0be48285837db01a481cd70ad1433
Poojit Maddineni <74945614+TinyAstroPsyco@users.noreply.github.com>
2026-04-04 00:53:18 -0400

    Add: buy-and-sell-crypto - submission-15

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-15.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int left = 0;
+        int right = 1;
+        int profit = 0;
+        while(right < prices.size()){
+            if(prices[left] <= prices[right]){
+                profit = max(profit, prices[right] - prices[left]);
+                right++;
+            }
+            else if(prices[right] < prices[left]){
+                left++;
+            }
+
+            
+            else right++;
+            
+
+        }
+        return profit;
+        
+    }
+};
